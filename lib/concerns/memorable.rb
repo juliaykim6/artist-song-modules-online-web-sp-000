9d4ce1bd1763da1e926f2e847842318acb336c7c
Julia Kim <juliaykim6@gmail.com>
2021-02-18 23:43:20 +0000

    Automatically backed up by Learn

--- lib/concerns/memorable.rb
+++ lib/concerns/memorable.rb
@@ -1,15 +1,11 @@
 module Memorable
   module ClassMethods
-    def self.reset_all
-      self.all.clear
+    def reset_all
+      all.clear
     end
 
-    def self.count
-      self.all.count
-    end
-
-    def all
-      self.all
+    def count
+      all.count
     end
   end
 
